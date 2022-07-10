f8336eb531b5addb4b91a9a83640a9ac03f0d142
Roman Sundurov <roman.sundurov.work@gmail.com>
2022-07-11 02:25:44 +0300

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Roman on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
